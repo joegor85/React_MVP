3ca453c70986dee95aa6d8e60ac0a72465162cd7
joegor85 <joe.gor85@gmail.com>
2023-05-04 17:42:44 -0500

    working route for single lists

--- server.js
+++ server.js
@@ -23,7 +23,30 @@ app.get("/api/lists", (req, res) => {
     });
 });
 
-
+app.get("/api/lists/:id", (req, res) => {
+    let id = req.params.id;
+    if (isNaN(id)) {
+        res.status(404).send("Enter a valid list id.");
+        return;
+    }
+    pool.query(`SELECT l.listname, json_agg(i.itemname) AS items
+    FROM lists l
+    JOIN items i ON l.list_id = i.list
+    WHERE l.list_id = $1
+    GROUP BY l.listname;`, [id]).then((result) => {
+        console.log(result);
+        if (result.rows.length === 0) {
+            res.status(404).send("This list doesn't exist.");
+            return;
+        } else {
+            let data = {
+                listname: result.rows[0].listname,
+                items: result.rows[0].items
+            };
+            res.send(data);
+        }
+    });
+});
 
 
 
