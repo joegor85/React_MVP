4bf51ae468547dd3c3a0425c723ae5df3300a764
joegor85 <joe.gor85@gmail.com>
2023-05-05 22:19:09 -0500

    can now add items to lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,94 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 import ListOfLists from "./ListOfLists";
 import ListDisplay from "./ListDisplay";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [lists, setLists] = useState([]);
   const [selectedList, setSelectedList] = useState(null);
   const [selectedListData, setSelectedListData] = useState([]);
-    
+
   const fetchTitleData = () => {
-      return fetch("http://localhost:3000/api/lists").then((response) => response.json()).then((data) => {
-          console.log(data);
-          setLists(data)});
-      }
-      
-      //fetching the list titles when the App loads
-      useEffect(() => {
-        fetchTitleData();
-      }, [])
-      
-      //Check to make sure I logged the right list_id when clicked
-      useEffect(() => {
-        console.log(selectedList);
-      }, [selectedList]);
-      
-      const fetchSpecificListData = () => {
-        return fetch(`http://localhost:3000/api/lists/${selectedList}`).then((response) => response.json()).then((listData) => {
-          setSelectedListData(listData);
-        })
-      }
+    return fetch("http://localhost:3000/api/lists")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setLists(data);
+      });
+  };
 
-      //Fetch data for a list when selected
-      useEffect(() => {
-        if (selectedList !== null) {
-          fetchSpecificListData();
-        }
-      }, [selectedList]);
+  //fetching the list titles when the App loads
+  useEffect(() => {
+    fetchTitleData();
+  }, []);
 
-      //Check to make sure I logged the right listData
-      useEffect(() => {
-      console.log(selectedListData);
-      }, [selectedListData]);
+  //Check to make sure I logged the right list_id when clicked
+  useEffect(() => {
+    console.log(selectedList);
+  }, [selectedList]);
 
+  const fetchSpecificListData = () => {
+    return fetch(`http://localhost:3000/api/lists/${selectedList}`)
+      .then((response) => response.json())
+      .then((listData) => {
+        setSelectedListData(listData);
+      });
+  };
+
+  //Fetch data for a list when selected
+  useEffect(() => {
+    if (selectedList !== null) {
+      fetchSpecificListData();
+    }
+  }, [selectedList]);
+
+  //Check to make sure I logged the right listData
+  useEffect(() => {
+    console.log(selectedListData);
+  }, [selectedListData]);
 
   return (
     <>
       <h1>Joe's List Manager</h1>
       <button>+</button>
-      < ListOfLists lists={lists} selectedList={selectedList} setSelectedList={setSelectedList}/>
+      <ListOfLists
+        lists={lists}
+        selectedList={selectedList}
+        setSelectedList={setSelectedList}
+      />
 
-      < ListDisplay selectedList={selectedList} selectedListData={selectedListData}/>
-
+      <ListDisplay
+        selectedList={selectedList}
+        selectedListData={selectedListData}
+        fetchSpecificListData={fetchSpecificListData}
+      />
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
 
 //Old Vite stuff:
-      // <div>
-      //   <a href="https://vitejs.dev" target="_blank">
-      //     <img src={viteLogo} className="logo" alt="Vite logo" />
-      //   </a>
-      //   <a href="https://react.dev" target="_blank">
-      //     <img src={reactLogo} className="logo react" alt="React logo" />
-      //   </a>
-      // </div>
-      // <h1>Vite + React</h1>
-      // <div className="card">
-      //   <button onClick={() => setCount((count) => count + 1)}>
-      //     count is {count}
-      //   </button>
-      //   <p>
-      //     Edit <code>src/App.jsx</code> and save to test HMR
-      //   </p>
-      // </div>
-      // <p className="read-the-docs">
-      //   Click on the Vite and React logos to learn more
-      // </p>
+// <div>
+//   <a href="https://vitejs.dev" target="_blank">
+//     <img src={viteLogo} className="logo" alt="Vite logo" />
+//   </a>
+//   <a href="https://react.dev" target="_blank">
+//     <img src={reactLogo} className="logo react" alt="React logo" />
+//   </a>
+// </div>
+// <h1>Vite + React</h1>
+// <div className="card">
+//   <button onClick={() => setCount((count) => count + 1)}>
+//     count is {count}
+//   </button>
+//   <p>
+//     Edit <code>src/App.jsx</code> and save to test HMR
+//   </p>
+// </div>
+// <p className="read-the-docs">
+//   Click on the Vite and React logos to learn more
+// </p>
--- src/ListDisplay.jsx
+++ src/ListDisplay.jsx
@@ -1,16 +1,48 @@
-
-
 const ListDisplay = (props) => {
-
+  //By defining this as a variable, I will not have to do this in the onClick call: () => addItem...
+  const addItem = async () => {
+    const list = props.selectedList;
+    let newItem = window.prompt("Enter a new item:");
+    try {
+    await fetch("http://localhost:3000/api/items", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        list: list,
+        itemName: newItem,
+      }),
+    });
+    await props.fetchSpecificListData();
+  } catch (error) {
+    console.error("Error adding item:", error);
+  }
+};
 
   return (
     <div className="listDisplay">
       {props.selectedList ? (
         <>
           <h2>List title: {props.selectedListData.listname}</h2>
-          <div className="listItemContainer">
-            {props.selectedListData.items.map((item) => <h3 key={item.id}>{item.name}</h3>)}
-          </div>
+
+          {/* This doesn't work if the data is too slow to come in, so I needed a buffer below */}
+          {/* <div className="listItemContainer">
+            {props.selectedListData.items.map((item) => (
+              <h3 key={item.id}>{item.name}</h3>
+            ))}
+          </div> */}
+
+          {/* ---------------My buffer code: */}
+          {props.selectedListData.items ? (
+            <div className="listItemContainer">
+              {props.selectedListData.items.map((item) => (
+                <h3 key={item.id}>{item.name}</h3>
+              ))}
+            </div>
+          ) : (
+            <div>Loading...</div>
+          )}
+          {/* -------------------------------- */}
+          <h3 className="addItem" onClick={addItem}>+</h3>
         </>
       ) : (
         <>
